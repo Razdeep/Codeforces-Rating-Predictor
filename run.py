c687072f7976bb42e5f6ac20c72865d894dc4c00
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2022-09-10 11:23:10 +0530

    fix: application port is now configurable with env var
    Signed-off-by: Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 from app import *
 import logging
+import os
 
 logging.basicConfig(level=logging.INFO)
 
+logger = logging.getLogger()
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=True)
+	port = os.getenv('PORT', 5000)
+	logger.info(f'Application is supposed to run on {port}')
+	app.run(host='0.0.0.0', port=port, debug=False)
